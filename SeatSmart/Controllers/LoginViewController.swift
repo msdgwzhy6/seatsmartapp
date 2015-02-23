9e64b3d6ced4c8c265a63a636a0d8f8daa2505c1
Outsider <outsider@Pats-iMac.local>
2015-02-23 12:02:10 -0600

    added forgot password view.  tweaks input fields

--- SeatSmart/Controllers/LoginViewController.swift
+++ SeatSmart/Controllers/LoginViewController.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var loginEmailField: UITextField!
+    @IBOutlet weak var loginPasswordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setFieldStyles()
+    }
 
+    func setFieldStyles() {
+        loginEmailField.textColor = UIColor.whiteColor()
+        loginPasswordField.textColor = UIColor.whiteColor()
+        
+        loginEmailField.placeholder = "Email"
+        loginPasswordField.placeholder = "Password"
+        
+        let loginEmailLeftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 45, height: 0))
+        loginEmailLeftPadding.backgroundColor = UIColor.clearColor()
+        loginEmailField.leftView = loginEmailLeftPadding
+        loginEmailField.leftViewMode = .Always
+        
+        let loginPasswordLeftPadding = UIView(frame: CGRect(x: 0, y: 0, width: 45, height: 0))
+        loginPasswordLeftPadding.backgroundColor = UIColor.clearColor()
+        loginPasswordField.leftView = loginPasswordLeftPadding
+        loginPasswordField.leftViewMode = .Always
         
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
